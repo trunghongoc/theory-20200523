c91098c9c7977f30362dde3e8a84e338dc204b2c
Trung Ho Ngoc - macpro 13 2018 <trung0194@gmai.com>
2020-05-23 20:43:36 +0700

    My init commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openMenuMobile(isOpen) {
+    const menu = window.document.getElementById('menu-mobile')
+
+    if (isOpen) {
+        menu.style.right = '0px'
+    } else {
+        menu.style.right = '-320px'
+    }
+}
